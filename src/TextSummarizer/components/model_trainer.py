a8f2eaceece9f6c56b8e710d7444f6d100f72ff7
Md Ibrahim Sikander <mdibrahimsikander@gmail.com>
2024-03-04 11:28:05 +0530

    Text Summarization ipynb file added

--- src/TextSummarizer/components/model_trainer.py
+++ src/TextSummarizer/components/model_trainer.py
@@ -25,7 +25,7 @@ class ModelTrainer:
         #     output_dir=self.config.root_dir, num_train_epochs=self.config.num_train_epochs, warmup_steps=self.config.warmup_steps,
         #     per_device_train_batch_size=self.config.per_device_train_batch_size, per_device_eval_batch_size=self.config.per_device_train_batch_size,
         #     weight_decay=self.config.weight_decay, logging_steps=self.config.logging_steps,
-        #     evaluation_strategy=self.config.evaluation_strategy, eval_steps=self.config.eval_steps, save_steps=1e6,
+        #     evaluation_strategy=self.config.evaluation_strategy, eval_steps=self.config.eval_steps, save_steps=self.config.save_steps,
         #     gradient_accumulation_steps=self.config.gradient_accumulation_steps
         # ) 
 
